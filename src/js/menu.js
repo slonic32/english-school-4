96703c898c0242f3c159fcd037a1995665fbe619
AnnetaDe <anneta.liss@gmail.com>
2025-03-21 15:38:11 +0100

    drop

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const mobileMenu = document.querySelector('.mob-menu')
+  const drop = document.querySelector('.drop')
   const mobMenuCloseBtn = document.querySelector('.mob-menu-button')
   const body = document.body
 
   mobMenuCloseBtn.addEventListener('click', () => {
     mobileMenu.classList.add('is-closed')
+    drop.classList.add('is-closed')
 
 
   })
